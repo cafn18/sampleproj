d96e6dcac77cb5513354ad217b10c9749aa8a609
cafn18 <126722383+cafn18@users.noreply.github.com>
2024-06-03 05:34:49 +0800

    Add files via upload

---
+++ sampleproj/src/main/java/com/mycompany/sampleproj/Authentication.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sampleproj;
+
+/**
+ *
+ * @author cafn
+ */
+public class Authentication {
+    public static boolean validateLogin(String username, String password) {
+        // This is a simple implementation that always returns true
+        return true;
+        }
+    
+    
+}
